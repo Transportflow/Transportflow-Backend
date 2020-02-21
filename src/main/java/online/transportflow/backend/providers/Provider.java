f252066b955ed625bccfa73de2ce23f659b1c76e
Adwirawien <hannsadrian@gmail.com>
2020-02-21 15:05:50 +0100

    Add Javadoc, use coordinates

--- src/main/java/online/transportflow/backend/providers/Provider.java
+++ src/main/java/online/transportflow/backend/providers/Provider.java
@@ -1,18 +1,37 @@
 package online.transportflow.backend.providers;
 
+import online.transportflow.backend.objects.Coordinates;
 import online.transportflow.backend.objects.Monitor;
-import online.transportflow.backend.objects.Stop;
+import online.transportflow.backend.objects.Location;
 import online.transportflow.backend.objects.UpcomingStop;
 
 import java.util.Date;
 import java.util.List;
 
 public interface Provider {
-
     String regionName = null;
 
-    List<Stop> searchStop(String input, int results, boolean stops, boolean addresses, boolean poi);
-    List<Stop> searchStop(long[] pos1, long[] pos2, int results, boolean stops, boolean addresses, boolean poi);
+    /**
+     * Returns a list of stops for a given query
+     *
+     * @param query      search string
+     * @param results    how many stops are max. returned
+     * @param stops      include stops?
+     * @param addresses  include addresses?
+     * @param poi        include poi's?
+     */
+    List<Location> searchLocation(String query, int results, boolean stops, boolean addresses, boolean poi);
+
+    /**
+     * Returns a list of stops for a radius around given coordinates
+     *
+     * @param coordinates  latitude & longitude for search
+     * @param radius       radius around coordinates in meters
+     * @param results      how many stops are max. returned
+     * @param stops        include stops?
+     * @param poi          include poi's?
+     */
+    List<Location> searchLocation(Coordinates coordinates, int radius, int results, boolean stops, boolean poi);
 
     Monitor getDepartures(String stopId, Date when, int duration);
     List<UpcomingStop> getNextStops(String tripId, String lineName, String currentStop);
