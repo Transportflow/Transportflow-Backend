6ec72cd2ec2ffee7f65a59aa609615eae67cbd62
Adrian <hannsadrian@gmail.com>
2020-03-17 08:45:33 +0100

    Add location search to api

--- src/main/java/online/transportflow/backend/Server.java
+++ src/main/java/online/transportflow/backend/Server.java
@@ -1,5 +1,13 @@
 package online.transportflow.backend;
 
+import com.google.gson.Gson;
+import online.transportflow.backend.objects.Location;
+import online.transportflow.backend.providers.BvgProvider;
+import online.transportflow.backend.providers.Provider;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 import static spark.Spark.*;
 
 public class Server {
@@ -9,25 +17,44 @@ public class Server {
         get("/hello", (req, res) -> "Transportflow API v0.1");
 
         before("/api/*", (req, res) -> {
-            if (req.queryParams("city") == null) {
-                halt(400, "No city specified");
+            if (req.queryParams("region") == null) {
+                halt(400, "No region specified");
                 return;
             }
 
-            String provider = null;
-            switch (req.queryParams("city")) {
+            Provider provider = null;
+            switch (req.queryParams("region").toLowerCase()) {
                 case "berlin":
-                    provider = "bvg";
+                case "brandenburg":
+                    provider = new BvgProvider(BvgProvider.getProducts());
+                    break;
             }
 
             if (provider == null) {
-                halt(400, "City not valid");
+                halt(400, "Region not valid");
                 return;
             }
 
+            res.type("application/json");
             req.attribute("provider", provider);
         });
 
-        get("/api/test", (req, res) -> req.attribute("provider"));
+        get("/api/locations", (req, res) -> {
+            Provider provider = req.attribute("provider");
+
+            String query = req.queryParams("query");
+            if (query == null) {
+                halt(400, "No query specified");
+                return null;
+            }
+
+            List<Location> loc = provider.searchLocation(query,
+                    req.queryParams("results") != null ? Integer.parseInt(req.queryParams("results")) : 7,
+                    req.queryParams("stops") != null && req.queryParams("stops").equals("true"),
+                    req.queryParams("address") != null && req.queryParams("address").equals("true"),
+                    req.queryParams("pois") != null && req.queryParams("pois").equals("true"));
+
+            return new Gson().toJson(loc);
+        });
     }
 }
