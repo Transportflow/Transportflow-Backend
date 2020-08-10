bf9475360b5367df618c29e2ad02befa55adf8cb
Hanns Adrian Böhme <hannsadrian@gmail.com>
2020-08-10 13:16:32 +0200

    Update RmvProvider.java

--- src/main/java/online/transportflow/backend/providers/regions/RmvProvider.java
+++ src/main/java/online/transportflow/backend/providers/regions/RmvProvider.java
@@ -5,7 +5,7 @@ import online.transportflow.backend.providers.DefaultLogos;
 
 // https://images.unsplash.com/photo-1542344807-157f76301e8a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3067&q=80
 public class RmvProvider {
-    public InsaProvider(List<Product> products) {
+    public RmvProvider(List<Product> products) {
         super("https://rmv.transportflow.online",
                 "Rhein-Main (RMV)",
                 "de",
@@ -16,12 +16,16 @@ public class RmvProvider {
 
     public static List<Product> getProviderProducts() {
         List<Product> products = new ArrayList<>();
-        products.add(new Product("nationalExpress", "ICE", DefaultLogos.getInterCityExpress()));
-        products.add(new Product("national", "IC/EC", DefaultLogos.getInterCity()));
-        products.add(new Product("regional", "RE/RB", DefaultLogos.getRegional()));
-        products.add(new Product("suburban", "S-Bahn", DefaultLogos.getSuburban()));
-        products.add(new Product("tram", "Tram", DefaultLogos.getTram()));
+        products.add(new Product("express-train", "ICE", DefaultLogos.getInterCityExpress()));
+        products.add(new Product("long-distance-train", "IC/EC", DefaultLogos.getInterCity()));
+        products.add(new Product("regiona-train", "RE/RB", DefaultLogos.getRegional()));
+        products.add(new Product("s-bahn", "S-Bahn", DefaultLogos.getSuburban()));
+        products.add(new Product("u-bahn", "U-Bahn", DefaultLogos.getSubway()));
+        products.add(new Product("tram", "Straßenbahn", DefaultLogos.getTram()));
+        products.add(new Product("watercraft", "Fähre", DefaultLogos.getFerry()));
         products.add(new Product("bus", "Bus", DefaultLogos.getBus()));
+        products.add(new Product("ast", "Anruf-Sammel-Taxi", DefaultLogos.getTaxi()));
+        products.add(new Product("cable-car", "Seilbahn", "https://www.dvb.de/assets/img/trans-icon/transport-lift.svg"));
         return products;
     }
 }
