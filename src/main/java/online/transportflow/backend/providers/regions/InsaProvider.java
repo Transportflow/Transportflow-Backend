2cc895064eba1cd5b0e0d51b443644c791201b9e
Hanns Adrian Böhme <hannsadrian@gmail.com>
2020-09-02 19:32:44 +0200

    Move INSA provider out of beta

--- src/main/java/online/transportflow/backend/providers/regions/InsaProvider.java
+++ src/main/java/online/transportflow/backend/providers/regions/InsaProvider.java
@@ -15,7 +15,7 @@ public class InsaProvider extends HafasProvider {
                 "https://images.unsplash.com/photo-1569663950244-5f2616a977a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
                 "white",
                 products);
-        super.beta = true;
+        super.beta = false;
     }
 
     public static List<Product> getProviderProducts() {
